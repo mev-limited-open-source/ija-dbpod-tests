47b6ced5413209fe2fbb80aee1286cb57ae0397d
Ian Abbott <abbotti@mev.co.uk>
2025-05-13 15:28:09 +0100

    recdata1_16_pulse: Change use of offsetof() for C standard compliance
    The result of offsetof() should be an integer constant expression, so
    using it to get the offset of an element of an array member can only be
    used if the index is an integer constant.

--- recdata1_16_pulse.c
+++ recdata1_16_pulse.c
@@ -398,8 +398,8 @@ static int do_pulse_config(unsigned index, unsigned len)
 
     m.cmd.hdr.dwSequence = cmdseq++;
     /* Round up to 4-byte boundary, 4 pulses per byte. */
-    m.cmd.hdr.dwLength = offsetof(DBPOD_CMDBUF_PULSE_CONFIG,
-            bData[((len + 15) & ~15) / 4]) - sizeof(ULONG);
+    m.cmd.hdr.dwLength = offsetof(DBPOD_CMDBUF_PULSE_CONFIG, bData[0]) -
+        sizeof(ULONG) + (((len + 15) & ~15) / 4);
     return do_cmd(&m.cmd.hdr);
 }
 
@@ -473,7 +473,8 @@ static int do_chan_configs(void)
 
     m.cmd.hdr.dwSequence = cmdseq++;
     m.cmd.hdr.dwLength =
-        offsetof(DBPOD_CMDBUF_CHAN_CONFIG, Gate[m.cmd.nGates]) - sizeof(ULONG);
+        offsetof(DBPOD_CMDBUF_CHAN_CONFIG, Gate[0]) - sizeof(ULONG) +
+        m.cmd.nGates * sizeof(m.cmd.Gate[0]);
     return do_cmd(&m.cmd.hdr);
 }
 
